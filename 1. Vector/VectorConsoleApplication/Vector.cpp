e16b2edb4bd7077517644dd974cf067574d3b282
Vadim <rsegtst>
2021-09-02 13:47:31 +0300

    feat: Vector. Fill Vector.h file

--- 1. Vector/VectorConsoleApplication/Vector.cpp
+++ 1. Vector/VectorConsoleApplication/Vector.cpp
@@ -1,1 +1,108 @@
 #include "Vector.h"
+
+Vector::Vector(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+	this->startPoint = new Vector::Point(x1, y1, z1);
+	this->endPoint = new Vector::Point(x2, y2, z2);
+}
+
+Vector::~Vector()
+{
+}
+
+double Vector::getLength()
+{
+	// TODO: insert return statement here
+}
+
+const Vector& Vector::operator+(const Vector& rhs)
+{
+	// TODO: insert return statement here
+}
+
+Vector& Vector::operator+=(const Vector& rhs)
+{
+	// TODO: insert return statement here
+}
+
+const Vector& Vector::operator-(const Vector& rhs)
+{
+	// TODO: insert return statement here
+}
+
+Vector& Vector::operator-=(const Vector& rhs)
+{
+	// TODO: insert return statement here
+}
+
+const Vector& Vector::operator*(const Vector& rhs)
+{
+	// TODO: insert return statement here
+}
+
+Vector& Vector::operator*=(const Vector& rhs)
+{
+	// TODO: insert return statement here
+}
+
+const Vector& Vector::operator*(const double& rhs)
+{
+	// TODO: insert return statement here
+}
+
+Vector& Vector::operator*=(const double& rhs)
+{
+	// TODO: insert return statement here
+}
+
+const Vector& Vector::operator/(const Vector& rhs)
+{
+	// TODO: insert return statement here
+}
+
+Vector& Vector::operator/=(const Vector& rhs)
+{
+	// TODO: insert return statement here
+}
+
+const Vector& Vector::operator^(const Vector& rhs)
+{
+	// TODO: insert return statement here
+}
+
+bool Vector::operator>(const Vector& rhs)
+{
+	return false;
+}
+
+bool Vector::operator>=(const Vector& rhs)
+{
+	return false;
+}
+
+bool Vector::operator<(const Vector& rhs)
+{
+	return false;
+}
+
+bool Vector::operator<=(const Vector& rhs)
+{
+	return false;
+}
+
+bool Vector::operator==(const Vector& rhs)
+{
+	return false;
+}
+
+bool Vector::operator!=(const Vector& rhs)
+{
+	return false;
+}
+
+Vector::Point::Point(double x, double y, double z)
+{
+	this->x = x;
+	this->y = y;
+	this->z = z;
+}
--- 1. Vector/VectorConsoleApplication/Vector.h
+++ 1. Vector/VectorConsoleApplication/Vector.h
@@ -3,7 +3,40 @@
 class Vector
 {
 public:
+	Vector(double x1, double y1, double z1, double x2, double y2, double z2);
+	~Vector();
+	double getLength();
+	const Vector& operator+(const Vector& rhs);
+	Vector& operator+=(const Vector& rhs);
+	const Vector& operator-(const Vector& rhs);
+	Vector& operator-=(const Vector& rhs);
+	const Vector& operator*(const Vector& rhs);
+	Vector& operator*=(const Vector& rhs);
+	const Vector& operator*(const double& rhs);
+	Vector& operator*=(const double& rhs);
+	const Vector& operator/(const Vector& rhs);
+	Vector& operator/=(const Vector& rhs);
+	const Vector& operator^(const Vector& rhs);
+	bool operator>(const Vector& rhs);
+	bool operator>=(const Vector& rhs);
+	bool operator<(const Vector& rhs);
+	bool operator<=(const Vector& rhs);
+	bool operator==(const Vector& rhs);
+	bool operator!=(const Vector& rhs);
 
 private:
+	struct Point
+	{
+	public:
+		friend class Vector;
+		Point(double x, double y, double z);
 
+	private:
+		double x;
+		double y;
+		double z;
+	};
+
+	Point *startPoint;
+	Point *endPoint;
 };
