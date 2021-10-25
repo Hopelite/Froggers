32b0fb9ccbd29df908076c5075a5c12783e72b1c
Vadim <rsegtst>
2021-10-25 14:46:57 +0300

    feat: Stack Machine. Begin implementing function reading logic

--- 3. Stack Machine/StackMachine/StackMachineFileReader.cpp
+++ 3. Stack Machine/StackMachine/StackMachineFileReader.cpp
@@ -2,15 +2,15 @@
 #include "StackMachineFileReader.h"
 #include "NoSuchFileException.h"
 #include <iostream>
+#include <map>
 
 StackMachineFileReader::StackMachineFileReader(const std::string& pathToFile)
 {
-	this->pathToFile = pathToFile;
-}
+	// TODO: Implement logic of skipping comments.
 
-void StackMachineFileReader::startReading()
-{
-	std::ifstream fileStream(this->pathToFile);
+	this->functions = new std::map<std::string, std::vector<std::string>>;
+
+	std::ifstream fileStream(pathToFile);
 	if (!fileStream.is_open())
 	{
 		throw NoSuchFileException();
@@ -18,46 +18,82 @@ void StackMachineFileReader::startReading()
 	else
 	{
 		std::string fileString;
-		int number = 0;
 		while (!fileStream.eof())
 		{
 			fileStream >> fileString;
+			if (fileString == "function")
+			{
+				fileStream >> fileString;
+				std::string currentString;
+				std::vector<std::string> functionBody;
+				fileStream >> currentString;
+				while (currentString != "{")
+				{
+					fileStream >> currentString;
+				}
 
-			if (fileString == "push")
-			{
-				fileStream >> number;
-				this->push(number);
-			}
-			else if (fileString == "pop")
-			{
-				this->pop();
-			}
-			else if (fileString == "peek")
-			{
-				this->peek();
-			}
-			else if (fileString == "dup")
-			{
-				this->duplicate();
-			}
-			else if (fileString == "add")
-			{
-				this->add();
-			}
-			else if (fileString == "sub")
-			{
-				this->subtract();
-			}
-			else if (fileString == "mul")
-			{
-				this->multiply();
-			}
-			else if (fileString == "div")
-			{
-				this->divide();
+				fileStream >> currentString;
+				while (currentString != "}")
+				{
+					functionBody.push_back(currentString);
+					fileStream >> currentString;
+				}
+
+				this->functions->insert(std::pair<std::string, std::vector<std::string>>(fileString, functionBody));
 			}
 		}
 	}
 
 	fileStream.close();
+}
+
+StackMachineFileReader::~StackMachineFileReader()
+{
+	delete this->functions;
+}
+
+void StackMachineFileReader::startReading()
+{
+	if (this->functions->find("main") == this->functions->end())
+	{
+		// TODO: Implement NoEntryPointException exception.
+	}
+
+	std::vector<std::string> mainBody = this->functions->find("main")->second;
+	for (size_t i = 0; i < mainBody.size(); i++)
+	{
+		if (mainBody[i] == "push")
+		{
+			int number = std::stoi(mainBody[++i]);
+			this->push(number);
+		}
+		else if (mainBody[i] == "pop")
+		{
+			this->pop();
+		}
+		else if (mainBody[i] == "peek")
+		{
+			this->peek();
+		}
+		else if (mainBody[i] == "dup")
+		{
+			this->duplicate();
+		}
+		else if (mainBody[i] == "add")
+		{
+			this->add();
+		}
+		else if (mainBody[i] == "sub")
+		{
+			this->subtract();
+		}
+		else if (mainBody[i] == "mul")
+		{
+			this->multiply();
+		}
+		else if (mainBody[i] == "div")
+		{
+			this->divide();
+		}
+	}
 }
--- 3. Stack Machine/StackMachine/StackMachineFileReader.h
+++ 3. Stack Machine/StackMachine/StackMachineFileReader.h
@@ -2,15 +2,18 @@
 #include <string>
 #include "StackMachine.h"
 #include <fstream>
+#include <vector>
+#include <map>
 
 class StackMachineFileReader : public StackMachine
 {
 public:
 	StackMachineFileReader(const std::string& pathToFile);
+	~StackMachineFileReader();
 
 	void startReading();
 
 private:
-	std::string pathToFile;
+	std::map<std::string, std::vector<std::string>>* functions;
 };
 
